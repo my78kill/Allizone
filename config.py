2c2069ea2c7781d046d460df84733b53b3ae8678
my78kill <aigirlfriend143you@gmail.com>
2026-03-20 23:54:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ DELETE_TIME = 10        # warning message delete
 EDIT_DELETE_TIME = 1800  # 30 min = 1800 sec
 
 # 🌐 NSFW API
-API_URL = "https://your-app.onrender.com/check"
+API_URL = "https://nsfw-81ex.onrender.com"
 API_KEY = "sk_92KjsH@8sKx_91!dkL"
